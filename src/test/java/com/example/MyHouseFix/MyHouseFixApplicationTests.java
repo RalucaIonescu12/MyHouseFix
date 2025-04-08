940a1550c8db6053a8b6599f0ea45e2a94c8ac0c
RalucaIonescu12 <ralucaandreeai@yahoo.com>
2025-04-08 21:24:30 +0300

    Initial commit

---
+++ src/test/java/com/example/MyHouseFix/MyHouseFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyHouseFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHouseFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
