394a748682db38a2027c88b558dbfe0a19e064a5
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2025-04-13 02:55:44 +0300

    First commit to security

---
+++ src/main/java/com/example/MyHouseFix/models/User.java
@@ -0,0 +1,91 @@
+package com.example.MyHouseFix.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table
+public class User implements UserDetails {
+    @Id
+    private int userId;
+
+    private String email;
+
+    private String fullName;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String fullName, String password) {
+        this.email = email;
+        this.fullName = fullName;
+        this.password = password;
+    }
+
+    public User(int userId, String email, String fullName, String password) {
+        this.userId = userId;
+        this.email = email;
+        this.fullName = fullName;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
