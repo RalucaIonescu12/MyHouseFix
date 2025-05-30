e196128426f238ef16661245fb71d85a1087b527
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2025-05-31 00:28:08 +0300

    Added post form

---
+++ src/client/src/components/FeedPageComponents/AddForm.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "../../FeedPage/FeedPageStyle.css";
+
+export const AddForm = ({cancelAdd, setSelectedFile, addPost}) => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const imageURL = URL.createObjectURL(file);
+        setSelectedFile(imageURL);
+    }
+  };
+
+  return (
+          <div className="add-form-display">
+            <div className="add-post-form">
+              <div onClick={cancelAdd} className="close-form">
+                <img src={hovered ? "/cancel-hover.svg" : "/cancel.svg"} 
+                  height="30px" width="30px" style={{cursor: "pointer"}}
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseLeave={() => setHovered(false)}
+                ></img>
+              </div>
+              <div style={{display: "flex", justifyContent: "center", marginTop: "-24px"}}>
+                <h2>Add New Post</h2>
+              </div>
+              <div className="add-form-elements">
+                <label htmlFor="name">Title</label>
+                <input type="text" id="name" />
+              </div>
+
+              <div className="add-form-elements">
+                <label htmlFor="description">Description</label>
+                <input type="text" id="description" />
+              </div>
+
+              <div className="add-form-elements">
+                <label htmlFor="price">Price</label>
+                <input type="text" id="price" />
+              </div>
+
+              <div className="add-form-elements">
+                <label htmlFor="photo">Photo</label>
+                <input 
+                  type="file" 
+                  id="photo" 
+                  accept="image/*" 
+                  onChange={handleFileChange}
+                />
+              </div>
+              <div className="add-post-button">
+                <button onClick={addPost} style={{width: "20%", backgroundColor: "#2c3e50", color:"white", borderRadius: "5px"}} id="addButton">
+                  Add post
+                </button>
+              </div>
+            </div>
+          </div>
+  );
+};
+export default AddForm;
